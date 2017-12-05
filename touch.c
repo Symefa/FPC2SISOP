0842edd79cadd0cea7523362811ee31b6994fb50
nm-mutia <nm.mutia02@gmail.com>
2017-12-05 14:21:36 +0700

    change 2->1 printf

--- touch.c
+++ touch.c
@@ -8,13 +8,13 @@ int main(int argc, char *argv[])
   int fd,i;
 
   if(argc < 2){
-    printf(2, "Use this arguments : touch [namefile]\n");
+    printf(1, "Use this arguments : touch [namefile]\n");
     exit();
   }
 
   for(i = 1; i < argc; i++){
     if((fd = open(argv[i], O_CREATE)) < 0){
-      printf(2, "failed create touch: %s \n", argv[i]);
+      printf(1, "failed create touch: %s \n", argv[i]);
       break;
     }
   }
